0abd1dff83bbcadd657b8c5123716a99b3347df0
Taeeun Kim <iostream1701@gmail.com>
2018-06-20 18:55:27 +0900

    aopcal 프로젝트 사전작업

---
+++ src/main/java/kr/hs/emirim/kimtae1701/aopcal/cal/Calculator.java
@@ -0,0 +1,7 @@
+package kr.hs.emirim.kimtae1701.aopcal.cal;
+
+public interface Calculator {
+
+	public long factorial(long num);
+
+}
---
+++ src/main/java/kr/hs/emirim/kimtae1701/aopcal/cal/RecCalculator.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.kimtae1701.aopcal.cal;
+
+public class RecCalculator implements Calculator {
+
+	@Override
+	public long factorial(long num) {
+		if (num == 0)
+			return 1;
+		else
+			return num * factorial(num - 1);
+	}
+
+}
